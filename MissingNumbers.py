1889b7f1e919b8b9dab7e8f466cfd8ed1edca6b8
Samuel Akuma <usakuma@uwaterloo.ca>
2023-01-25 14:47:58 -0500

    Solution for https://www.hackerrank.com/challenges/missing-numbers/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign

---
+++ MissingNumbers.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import os
+
+
+#
+# Complete the 'missingNumbers' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY arr
+#  2. INTEGER_ARRAY brr
+#
+
+def missingNumbers(arr, brr):
+    for element in arr:
+        brr.remove(element)
+
+    return sorted(set(brr))
+
+
+if __name__ == '__main__':
+    fptr = open("/Users/samuelakuma/Desktop/file.txt", 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    m = int(input().strip())
+
+    brr = list(map(int, input().rstrip().split()))
+
+    result = missingNumbers(arr, brr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
