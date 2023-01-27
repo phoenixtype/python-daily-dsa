6754f3444d6394e747036fc6e65b7ba19b276061
Samuel Akuma <usakuma@uwaterloo.ca>
2023-01-26 23:14:02 -0500

    Solution for https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true

---
+++ VeryBigSum.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Write your code here
+    add = 0
+    for i in range(len(ar)):
+        add += ar[i]
+    return add
+
+
+if __name__ == '__main__':
+    fptr = open("/Users/samuelakuma/Desktop/file.txt", 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
